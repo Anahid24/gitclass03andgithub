a72fdea54d579de5f2540dd437159d4bc319a7a8
Anahid24 <anahidgasparyan03@gmail.com>
2022-05-31 21:38:17 -0700

    2nd commit

--- src/github/gitAndGitHub.java
+++ src/github/gitAndGitHub.java
@@ -4,7 +4,8 @@ public class gitAndGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("today we learned github");
+		System.out.print("today we learned github");
+		System.out.print("today is a very good day");
 
 	}
 
